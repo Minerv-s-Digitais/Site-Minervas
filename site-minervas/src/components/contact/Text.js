3a18b8d957c11af3292e15bd39387c461a1eb04b
Victor Santos <victorpss@dcc.ufrj.br>
2023-07-19 15:04:44 -0300

    Adicionado conteúdo em Minervas nas Redes, ícones de telefone e email e mapa do CCMN

--- site-minervas/src/components/contact/Text.js
+++ site-minervas/src/components/contact/Text.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Text.scss";
+import phone from "../../assets/phone-draw.svg"
+import mail from "../../assets/mail-draw.svg"
 
 class Text extends React.Component {
   constructor(props) {
@@ -15,12 +17,16 @@ class Text extends React.Component {
         <div className="title">Contato</div>
         <div className="section-icons-contact"> 
           <div className="icons-contact-collum1">
-            <div className="icone1-contact"></div>
+            <div className="icone1-contact">
+              <img className="icone1-contact" src={phone} alt='desenho do telefone'></img>
+            </div>
             <p className="general-phone-number">(22)9999-9999</p>       
           </div>
           <div className="icons-contact-collum2">
-            <div className="icone2-contact"></div>
-            <p className="general-email-address">email@ufrj.com</p>       
+            <div className="icone2-contact">
+              <img className="icone2-contact" src={mail} alt='desenho do e-mail'></img>
+            </div>
+            <p className="general-email-address">minervasdigitais@dcc.ufrj.br</p>       
           </div>
         </div>
         <div className="section-advisor-infos">
@@ -33,7 +39,18 @@ class Text extends React.Component {
             <div className="advisor-icons3"></div>
           </div>
         </div>
-        <div className="section-maps-content"></div>
+        <div className="section-maps-content">
+        <iframe 
+          title='Mapa do Google'
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3676.5175938242537!2d-43.22904841595763!3d-22.8573305930057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9979386e95f2e1%3A0x20a7d1ce2f645285!2sCentro%20de%20Ci%C3%AAncias%20Matem%C3%A1ticas%20e%20da%20Natureza%20(CCMN)%20-%20UFRJ!5e0!3m2!1spt-BR!2sbr!4v1676236261560!5m2!1spt-BR!2sbr" 
+          width="600" 
+          height="450" 
+          style={{border: 0}} 
+          allowfullscreen="" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
 
       </div>
     );
--- site-minervas/src/components/net/Net.js
+++ site-minervas/src/components/net/Net.js
@@ -1,6 +1,15 @@
 import React, { useRef } from "react"
 
 import exampleimage from "../../assets/example-image.svg"
+import photo01 from "../../assets/insta-photo01.jpg"
+import photo02 from "../../assets/insta-photo02.jpg"
+import photo03 from "../../assets/insta-photo03.jpg"
+import photo04 from "../../assets/insta-photo04.jpg"
+import photo05 from "../../assets/insta-photo05.jpg"
+import photo06 from "../../assets/insta-photo06.jpg"
+import photo07 from "../../assets/insta-photo07.jpg"
+import photo08 from "../../assets/insta-photo08.jpg"
+import photo09 from "../../assets/insta-photo09.jpg"
 import Text from "./Text"
 
 import "./Net.scss"
@@ -8,29 +17,34 @@ import "./Net.scss"
 function Net() {
   const ref = useRef(null);
 
+  const redirectToLink = (link) => {
+    window.open(link, '_blank')
+  }
+
   const handleClick = () => {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   }
   return (
     <div>
       <div className="net-content">
+        
         <Text click={() => handleClick()}  />
         <div style={{ width: '98vw', display: 'flex', justifyContent: 'center'}}>
-        <div style={{border: 'solid 2x red', width: '50%'}}>
+        <div style={{border: 'solid 2x red', width: '50%', cursor: 'pointer'}}>
           <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={exampleimage} className="line1" alt="example1" />
-            <img src={exampleimage} className="line1" alt="example2" />
-            <img src={exampleimage} className="line1" alt="example3" />
+            <img src={photo01} className="line1" alt="photo01" onClick={() => redirectToLink('https://www.instagram.com/p/CuCr_g5rX9b/') }/>
+            <img src={photo02} className="line1" alt="photo02" onClick={() => redirectToLink('https://www.instagram.com/p/CtpsizaJxTx/?img_index=1') }/>
+            <img src={photo03} className="line1" alt="photo03" onClick={() => redirectToLink('https://www.instagram.com/p/Csrsz-MpEsX/') }/>
           </div>
           <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={exampleimage} className="line2" alt="example4" />
-            <img src={exampleimage} className="line2" alt="example5" />
-            <img src={exampleimage} className="line2" alt="example6" />
+            <img src={photo04} className="line2" alt="photo04" onClick={() => redirectToLink('https://www.instagram.com/p/CsHMPYqJ3bl/?img_index=1') }/>
+            <img src={photo05} className="line2" alt="photo05" onClick={() => redirectToLink('https://www.instagram.com/p/Cr3awq5rmUQ/') }/>
+            <img src={photo06} className="line2" alt="photo06" onClick={() => redirectToLink('https://www.instagram.com/p/CqqbXhnJ2ws/') }/>
           </div>
           <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={exampleimage} className="line3" alt="example7" />
-            <img src={exampleimage} className="line3" alt="example8" />
-            <img src={exampleimage} className="line3" alt="example9" />
+            <img src={photo07} className="line3" alt="photo07" onClick={() => redirectToLink('https://www.instagram.com/p/CqeTtR-r6lY/') }/>
+            <img src={photo08} className="line3" alt="photo08" onClick={() => redirectToLink('https://www.instagram.com/p/CqT6cIQpGdq/?img_index=1') }/>
+            <img src={photo09} className="line3" alt="photo09" onClick={() => redirectToLink('https://www.instagram.com/p/CqJsfOAJyuw/?img_index=1') }/>
           </div>
         </div>
 
