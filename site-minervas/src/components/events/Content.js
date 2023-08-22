d69efb2cacc7bdac8193b6841bc95359a8bc3918
Rayane Domingos <rayane@ejcm.com.br>
2023-08-22 20:01:12 -0300

    o botão eventos do header não estava indo para a parte de eventos

--- site-minervas/src/components/events/Content.js
+++ site-minervas/src/components/events/Content.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useRef } from "react"
 import "./Content.scss"
 import Carousel1 from "./Gallery1"
 import Carousel2 from "./Gallery2"
 import TextEvent from "./Text"
 
 function Events() {
+    const ref = useRef(null);
+
+    const handleClick = () => {
+        ref.current?.scrollIntoView({ behavior: 'smooth' });
+    }
 
     return (
-        <div className="events-container">
+        <div className="events-container"  id="Events">
             <TextEvent />
             <div className="gallery-container">
                 <div className="gallery-transform" >
