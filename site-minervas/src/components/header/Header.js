f5dc680ff1e6e558b1d014d7c48c7a0a66a95d74
Victor Santos <victorpss@dcc.ufrj.br>
2023-09-12 21:59:05 -0300

    nf - ícone sanduíche para tela <=970px, incompleto

--- site-minervas/src/components/header/Header.js
+++ site-minervas/src/components/header/Header.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa"; // Importe o ícone de sanduíche
 
 import logonova from "../../assets/logonova-completa.svg"
 import Menu from "./Menu"
@@ -6,9 +7,14 @@ import Menu from "./Menu"
 import "./Header.scss"
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header">
       <img src={logonova} className="logo" alt="logo" />
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        <FaBars />
+      </div>
       <Menu />
     </div>
   )
