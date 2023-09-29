169ec0238639f37205e57980eaa79c9e12d01d65
Rafael Ribeiro <rafael_oliribeiro@yahoo.com.br>
2023-09-29 20:55:06 -0300

    feat: Responsividade seção - Minervas nas Redes - do site

--- site-minervas/src/components/net/Net.js
+++ site-minervas/src/components/net/Net.js
@@ -1,18 +1,25 @@
 import React, { useRef } from "react"
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-//import exampleimage from "../../assets/example-image.svg"
 import photo18082023 from "../../assets/insta-photo18-08-2023.jpg"
 import photo10082023 from "../../assets/insta-photo10-08-2023.jpg"
 import photo26072023 from "../../assets/insta-photo26-07-2023.jpg"
 import photo28062023 from "../../assets/insta-photo28-06-2023.jpg"
 import photo18062023 from "../../assets/insta-photo18-06-2023.jpg"
 import photo25052023 from "../../assets/insta-photo25-05-2023.jpg"
-import photo11052023 from "../../assets/insta-photo11-05-2023.jpg"
-import photo05052023 from "../../assets/insta-photo05-05-2023.jpg"
-import photo05042023 from "../../assets/insta-photo05-04-2023.jpg"
 import Text from "./Text"
 
 import "./Net.scss"
+
+const Item = styled(Paper)(({ theme }) => ({
+  height: "260px",
+  textAlign: 'center',
+  justifyContent: 'center',
+  backgroundColor: 'transparent',
+  boxShadow: "none",
+}));
 
 function Net() {
   const ref = useRef(null);
@@ -25,38 +32,44 @@ function Net() {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   }
   return (
-    <div>
+    <div className="net-content-background">
       <div className="net-content">
-        {/* Ter cuidado de, na hora de atualizar o conteúdo, passar o link de uma determinada linha para a linha de baixo, 
-        e na primeira linha colocar o link da foto nova no Instagram. Além do link, atualizar também as 'src'. */}
         <Text click={() => handleClick()}  />
-        <div style={{ width: '98vw', display: 'flex', justifyContent: 'center'}}>
-        <div style={{border: 'solid 2x red', width: '50%', cursor: 'pointer'}}>
-          <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={photo18082023} className="line1" alt="photo01" onClick={() => redirectToLink('https://www.instagram.com/p/CwGkaa_JI5H/') }/>
-            <img src={photo10082023} className="line1" alt="photo02" onClick={() => redirectToLink('https://www.instagram.com/p/CvyLVx9JO5Z/?img_index=1') }/>
-            <img src={photo26072023} className="line1" alt="photo03" onClick={() => redirectToLink('https://www.instagram.com/p/CvLWGrIpfJn/') }/>
-          </div>
-          <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={photo28062023} className="line2" alt="photo04" onClick={() => redirectToLink('https://www.instagram.com/p/CuCr_g5rX9b/') }/>
-            <img src={photo18062023} className="line2" alt="photo05" onClick={() => redirectToLink('https://www.instagram.com/p/CtpsizaJxTx/?img_index=1') }/>
-            <img src={photo25052023} className="line2" alt="photo06" onClick={() => redirectToLink('https://www.instagram.com/p/Csrsz-MpEsX/') }/>
-          </div>
-          <div style={{display: "flex", justifyContent: 'center'}}>
-            <img src={photo11052023} className="line3" alt="photo07" onClick={() => redirectToLink('https://www.instagram.com/p/CsHMPYqJ3bl/?img_index=1') }/>
-            <img src={photo05052023} className="line3" alt="photo08" onClick={() => redirectToLink('https://www.instagram.com/p/Cr3awq5rmUQ/') }/>
-            <img src={photo05042023} className="line3" alt="photo09" onClick={() => redirectToLink('https://www.instagram.com/p/CqqbXhnJ2ws/') }/>
-          </div>
+        <div className="imagens-grid-net">
+          <Grid  container spacing={5}>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo18082023} className="line1" alt="post-minervas1" onClick={() => redirectToLink('https://www.instagram.com/p/CwGkaa_JI5H/') }/>
+              </Item>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo10082023} className="line1" alt="post-minervas2" onClick={() => redirectToLink('https://www.instagram.com/p/CvyLVx9JO5Z/?img_index=1') }/>
+              </Item>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo26072023} className="line1" alt="post-minervas3" onClick={() => redirectToLink('https://www.instagram.com/p/CvLWGrIpfJn/') }/>
+              </Item>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo28062023} className="line1" alt="post-minervas4" onClick={() => redirectToLink('https://www.instagram.com/p/CuCr_g5rX9b/') }/>
+              </Item>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo18062023} className="line1" alt="post-minervas5" onClick={() => redirectToLink('https://www.instagram.com/p/CtpsizaJxTx/?img_index=1') }/>
+              </Item>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
+              <Item>
+                <img src={photo25052023} className="line1" alt="post-minervas6" onClick={() => redirectToLink('https://www.instagram.com/p/Csrsz-MpEsX/') }/>
+              </Item>
+            </Grid>
+          </Grid>
         </div>
-
-        </div>
-        
-
-
       </div>
-      {/* <div ref={ref}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div> */}
     </div>
   )
 }
--- site-minervas/src/components/net/Text.js
+++ site-minervas/src/components/net/Text.js
@@ -1,24 +1,6 @@
 import React from "react"
-
-import Button from "@material-ui/core/Button"
-import { withStyles } from "@material-ui/core/styles"
-import { redirect } from "react-router-dom"
-
 import "./Text.scss"
 
-const ColorButton = withStyles(() => ({
-  root: {
-    textTransform: "none",
-    borderRadius: 55,
-    fontFamily: "Lato",
-    fontWeight: 300,
-    backgroundColor: "#096262",
-    "&:hover": {
-      backgroundColor: "#13BADE",
-      fontWeight: "bold",
-    },
-  },
-}))(Button)
 
 class Text extends React.Component {
 
@@ -29,16 +11,7 @@ class Text extends React.Component {
     }
   }
 
-  openChat = () => {
-    this.setState({
-      openChat: true
-    })
-  }
-
   render(){
-    const {click} = this.props
-    console.log(this.props)
-
     return (
       <div className="text">
         <div className="title">
