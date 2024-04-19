b12badc2a9354a80cea6deb7f1524bff8a8e0fcd
victorpss <victorpss@dcc.ufrj.br>
2024-04-18 23:52:30 -0300

    feat: actions section

---
+++ site-minervas/src/pages/Actions/Actions.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Header from "../../components/header/Header"
+import ActionsContent from "../../components/actions/content/ActionsContent"
+import Materials from "../../components/actions/materials/Materials"
+import Footer from "../../components/footer/Footer"
+
+import "./Actions.scss"
+
+function Actions() {
+  return (
+    <div className="actions" id="#">
+      <Header />
+        <div>alooooo</div>
+        <div><ActionsContent /></div>
+        <div><Materials /></div>
+        <div style={{ height: 200 }}></div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Actions
