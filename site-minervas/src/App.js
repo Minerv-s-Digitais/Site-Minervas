33aa734d73cee1c30e5e2de87d3e928e46088bc4
Rafael Ribeiro <rafael_oliribeiro@yahoo.com.br>
2024-09-03 20:53:49 -0300

    fix: altera BrowserRouter para HasRouter para deploy Gh-pages

--- site-minervas/src/App.js
+++ site-minervas/src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import Home from '../src/pages/Home/Home'
 import Actions from '../src/pages/Actions/Actions'
 import Sobre from '../src/pages/Sobre/Sobre'
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/acoes" element={<Actions />} />
           <Route path="/sobre" element={<Sobre />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </React.Fragment>
   );
 }
