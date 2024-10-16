337c5ba4883f3729b8300316ed5b3ecd01378a1e
Lorena Mamede <loremamede@gmail.com>
2024-10-16 14:29:46 -0300

    [DevOps] Criando ambiente de deploy para server
    - Usando containers para facilitar CI/CD
    - Alterando a forma como o router funciona para facilitar uso do nginx
    - Configurando Nginx como server da aplicação

--- site-minervas/src/App.js
+++ site-minervas/src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import Home from '../src/pages/Home/Home'
 import Actions from '../src/pages/Actions/Actions'
 import Sobre from '../src/pages/Sobre/Sobre'
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
-      <HashRouter basename='/'>
+      <BrowserRouter basename='/'>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/acoes" element={<Actions />} />
           <Route path="/sobre" element={<Sobre />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
